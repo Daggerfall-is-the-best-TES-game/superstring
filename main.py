2d7bfff2723740025bf61b7ddf52dbdde9c3c504
Daggerfall-Is-The-Best-TES-Game <Algoalt904@gmail.com>
2019-06-29 02:49:27 -0400

    added an entry in the letter scores dictionary called "?" that represents wildcards

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ valid_scrabble_words = {word.strip() for word in open('enable1.txt', 'r')}
 
 def string_score(solution):
     def score(word):
-        letter_scores = {'e': 1, 'a': 1, 'i': 1, 'o': 1, 'n': 1, 'r': 1, 't': 1, 'l': 1, 's': 1, 'u': 1,
+        letter_scores = {'?': 0,
+                         'e': 1, 'a': 1, 'i': 1, 'o': 1, 'n': 1, 'r': 1, 't': 1, 'l': 1, 's': 1, 'u': 1,
                          'd': 2, 'g': 2,
                          'b': 3, 'c': 3, 'm': 3, 'p': 3,
                          'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
@@ -12,4 +13,5 @@ def string_score(solution):
                          'q': 10, 'z': 10}
         return sum(letter_scores[letter] for letter in word)
     return sum(score(word) for word in valid_scrabble_words if word in solution)
+
 
