ffbcd8de0a5604eb0c6f7c61e327a19bb3188522
Daggerfall-Is-The-Best-TES-Game <Algoalt904@gmail.com>
2019-06-29 04:37:26 -0400

    implemented a really greedy strategy to get a baseline algorithm

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from random import sample
+
+
 valid_scrabble_words = {word.strip() for word in open('enable1.txt', 'r')}
+scrabble_tile_frequencies = {'?': 2,
+                             'e': 12, 'a': 9, 'i': 9, 'o': 8, 'n': 6, 'r': 6, 't': 6, 'l': 4, 's': 4, 'u': 4,
+                             'd': 4, 'g': 3,
+                             'b': 2, 'c': 2, 'm': 2, 'p': 2,
+                             'f': 2, 'h': 2, 'v': 2, 'w': 2, 'y': 2,
+                             'k': 1,
+                             'j': 1, 'x': 1,
+                             'q': 1, 'z': 1}
+scrabble_tiles = [tile for tile in scrabble_tile_frequencies for x in range(scrabble_tile_frequencies[tile])]
 
 
 def string_score(solution):
@@ -12,6 +24,20 @@ def string_score(solution):
                          'j': 8, 'x': 8,
                          'q': 10, 'z': 10}
         return sum(letter_scores[letter] for letter in word)
-    return sum(score(word) for word in valid_scrabble_words if word in solution)
+
+    return sum(score(word) for word in valid_scrabble_words if word in solution)  #TODO: word in solution must be modified to account for wildcards
 
 
+test_solution = ""
+while scrabble_tiles:
+    best_tile = scrabble_tiles[0]
+    for tile in set(scrabble_tiles):
+        new_solution = test_solution + tile
+        if string_score(new_solution) > string_score(test_solution):
+            best_tile = tile
+    test_solution += best_tile
+    scrabble_tiles.remove(best_tile)
+    print(test_solution)
+
+
+print(string_score(test_solution))
